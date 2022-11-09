b3cd5e2a3c3d3f68fd1d39b767422563ee7d5d9f
Smede NSONA Mass <104262940+Ezochetms@users.noreply.github.com>
2022-11-09 18:57:31 +0100

    Create 0xdFB6ED3237ee9A3777cD4A46E0C14D7F798ed3fF.js

---
+++ 0xdFB6ED3237ee9A3777cD4A46E0C14D7F798ed3fF.js
@@ -0,0 +1,19 @@
+"Contract" : "0xdFB6ED3237ee9A3777cD4A46E0C14D7F798ed3fF" ;
+"Nom": "ethereum" ;
+"Symbol": "eth" ;
+"Decimal": 18" ;
+"Suplly" : 21000000" ;
+"Prix" : "1eth" ;
+"Network" : "goerli test network uniquement" ;
+"Type" : "sample erc20 to" ;
+"Propriétaire" : "ezochetms@gmail.com" ;
+"Logo" : "https://ibb.co/bHbztGL"
+"Verification link" : "https://goerli.etherscan.io/tokens?q=0xdFB6ED3237ee9A3777cD4A46E0C14D7F798ed3fF" ;
+//fonncton
+{
+"Interface ERC-165" : "OUI" ;
+"Assistance ERC-721Assistance ERC-721" : "NON" ;
+"Prise en charge des métadonnées ERC-721Prise en charge des métadonnées ERC-721" : "NON" ;
+"Prise en charge énumérable ERC-721Prise en charge énumérable ERC-721" : "NON" ;
+Implémente ERC-721 : "NON" ;
+}
